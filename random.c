04b5c015a2a02c35bf32fa26211efe4a0d9d5947
Jude Piacentino <tuk97150@temple.edu>
2023-01-20 16:43:22 -0500

    implemented randomly selecting a letter by using ascii values and printing the character value of the randomly generated number, still need to compile it and get the command recognized on the main program

--- random.c
+++ random.c
@@ -1,64 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include "random.h"
 
 
 int randchar(){// randchar which will be used to generate random character from a-z
-int a = 65;
-int b = 66;
-int c = 67;
-int d = 68;
-int e = 69;
-int f = 70;
-int g = 71;
-int h = 72;
-int i = 73;
-int j = 74;
-int k = 75;
-int l = 76;
-int m = 77;
-int n = 78;
-int o = 79;
-int p = 80;
-int q = 81;
-int r = 82;
-int s = 83;
-int t = 84;
-int u = 85;
-int v = 86;
-int w = 87;
-int x = 88;
-int y = 89;
-int z = 90;
 
-char A = (char)a;
-char B = (char)b;
-char C = (char)c;
-char D = (char)d;
-char E = (char)e;
-char F = (char)f;
-char G = (char)g;
-char H = (char)h;
-char I = (char)i;
-char J = (char)j;
-char K = (char)k;
-char L = (char)l;
-char M = (char)m;
-char N = (char)n;
-char O = (char)o;
-char P = (char)p;
-char Q = (char)q;
-char R = (char)r;
-char S = (char)s;
-char T = (char)t;
-char U = (char)u;
-char V = (char)v;
-char W = (char)w;
-char X = (char)x;
-char Y = (char)y;
-char Z = (char)z;
+int lower = 65, upper = 90;
 
-printf("%c %c %C", A, Z);
+   srand(time(0));
+
+   printf("Random letter: ");
+   for(int i = 0; i < 1; i++){
+    int num = (rand() % (upper - lower + 1)) + lower;
+    printf("%c", (char)num);
+   }
+
+    return (0);
 
 }
 int main(){
